3089bad1332a25862728dd6a625ba67617e9719e
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-09 03:17:06 +0800

    support for stopping inet services

---
+++ manifests/disable_inetd_service.pp
@@ -0,0 +1,15 @@
+# Aix_tidy::Disable_inetd_service
+#
+# Use chsubserver to disable a list of inetd services
+#
+# @param Array of services to disable, see
+#   https://forge.puppet.com/geoffwilliams/chsubserver/readme for information on
+#   titleformats the puppet chsubserver type accepts
+class aix_tidy::disable_inetd_service(
+    Array[String] $disable = []
+) {
+
+  chsubserver { $disable:
+    ensure => disabled
+  }
+}
---
+++ spec/classes/disable_inetd_service_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+describe 'aix_tidy::disable_inetd_service' do
+  context 'with default values for all parameters' do
+    it { should contain_class('aix_tidy::disable_inetd_service') }
+  end
+
+  context 'processes passed in list of services correctly' do
+    let :params do
+      {
+        :disable => ["foo->bar", "baz->bas"],
+      }
+    end
+    it { should contain_class('aix_tidy::disable_inetd_service') }
+  end
+end
