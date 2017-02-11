d7fb647fdf14417f5ff80b223b539440ee7edc86
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-11 17:07:13 +0800

    remove duplicated functionality, relase

--- manifests/hosts_equiv.pp
+++ manifests/hosts_equiv.pp
@@ -1,12 +1,23 @@
-# Aix_tidy::Hosts_equiv
+# Aix_tidy::hosts_equiv
 #
-# Disable hosts_equiv by installing an empty file
+# Disable all hosts.equiv entries by commenting them out
 class aix_tidy::hosts_equiv {
+
+  # no sed -i on AIX so must move manually..sigh
+  $fix_cmd = 'sed \'s/\(^[^#].*$\)/# \1/\' /etc/hosts.equiv  > /etc/hosts.equiv.work && mv hosts.equiv.work hosts.equiv '
+  $test_cmd = 'grep ^[^#] /etc/hosts.equiv'
+
+  exec { "fix_hosts_equiv":
+    command => $fix_cmd,
+    onlyif  => $test_cmd,
+    path    => ['/usr/bin','/bin'],
+  }
+
   file { "/etc/hosts.equiv":
     ensure  => file,
     owner   => "root",
     group   => "system",
     mode    => "0644",
-    content => "# disabled by puppet"
+    require => Exec[fix_hosts_equiv],
   }
 }
--- spec/classes/disable_hosts_equiv_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-describe 'aix_tidy::disable_hosts_equiv' do
-  context 'with default values for all parameters' do
-    it { should contain_class('aix_tidy::disable_hosts_equiv') }
-  end
-end
